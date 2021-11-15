85c89db9331fada2bad838c517f4d810a78e3a79
John Ortiz <jaortiz92@hotmail.com>
2021-11-15 17:23:04 -0500

    Serializers User app Cesar

---
+++ userProject/userApp/Serializers/userSerializer.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from userApp.models.user import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id_user', 'name', 'document', 'email',
+                  'password', 'birth_date', 'cellphone', 'gender']
+
+    def create(self, validated_data):
+
+        userInstance = User.objects.create(**validated_data)
+
+        return userInstance
+
+    def to_representation(self, obj):
+        user = User.objects.get(id=obj.id)
+
+        return {
+            'id_user': user.id_user,
+            'name': user.name,
+            'document': user.document,
+            'password': user.password,
+            'birth_date': user.birth_date,
+            'cellphone': user.cellphone,
+            'gender': user.gender
+        }
