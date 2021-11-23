a04d053055663dd1d1ed3833e230bc9311644277
“DjgutiA” <david.j.gutierrez.a@gmail.comgit clone https://DjgutiA@bitbucket.org/DjgutiA/test.gitGIT config --global user.email david.j.gutierrez.a@gmail.com>
2021-11-23 10:17:42 -0500

    .

--- carProject/carApp/models/cities.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Cities(models.Model):
-    id_city = models.AutoField(primary_key=True)
-    name_city = models.CharField(max_length=45, verbose_name='City', null=False)
